be1379d0030030dbc41feb96087109f3a74b030a
学生服务器3090 <2689236030@qq.com>
2026-05-18 14:50:54 +0800

    适配数据集的修改

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Apr 18 21:35:20 2022
-__version__ = '1.2.0+386ca20'
+# TIME: Mon May 18 14:45:59 2026
+__version__ = '1.2.0+81acf8c'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
